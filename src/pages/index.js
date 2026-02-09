d267f7dc74eafe7436152016336c9a564cfe5ab7
omarderas <bz.inlandtours@gmail.com>
2026-02-08 21:59:57 -0600

    new updates

--- src/pages/index.js
+++ src/pages/index.js
@@ -14,6 +14,7 @@ import Picks from "./home/components/picks"
 import Reviews from "./home/components/reviews"
 const IndexPage = () => (
   <Layout>
+      <Seo title="St Herman's Cave Belize" />
      <Hero /> 
      <Tours />
      <Welcome />
@@ -23,13 +24,6 @@ const IndexPage = () => (
      <Reviews />
   </Layout>
 )
-
-/**
- * Head export to define metadata for the page
- *
- * See: https://www.gatsbyjs.com/docs/reference/built-in-components/gatsby-head/
- */
-export const Head = () => <Seo title="St Herman's Cave Belize" />
 
 export default IndexPage
                
